b69165c21314da27897914d3282d059109551baf
Amia Defreitas <amia.defreitas0@gmail.com>
2019-10-23 17:29:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-   first_number = 4
-   second_number = 4
+   first_number = 6
+   second_number = 6
 
 sum = first_number + second_number
 difference = first_number - second_number
